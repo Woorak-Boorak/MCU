7e6044831e7fea670b1a53e5020fa2f2917ff530
kwonyoungmin13 <kym115677@gmail.com>
2025-09-19 23:11:29 +0900

    Update ADC_Init.c

--- GccApplication1/ReadAdc/ADC_Init.c
+++ GccApplication1/ReadAdc/ADC_Init.c
@@ -6,9 +6,16 @@
  */
 
 #include "ADC_Init.h"
+#define STEER 3
+#define MOTOR 4
+#define BREAK 5
 
-static volatile uint16_t adc_values[3] = {0, 0, 0};
-static volatile uint8_t current_channel = 3;  // 시작: ADC3
+
+static volatile uint16_t steer;
+static volatile uint16_t motor;
+static volatile uint16_t brek;
+static volatile uint16_t light;
+static volatile uint8_t current_channel = STEER;  // 시작: ADC3
 
 void ADC_Init(void) {
 	ADMUX = (1 << REFS0) | (current_channel & 0x07); // AVcc 기준, ADC3 선택
@@ -16,21 +23,34 @@ void ADC_Init(void) {
 	ADCSRA |= (1 << ADSC); // 첫 변환 시작
 }
 
-uint16_t ADC_GetValue(uint8_t idx) {
-	// idx=0 → ADC3, idx=1 → ADC4, idx=2 → ADC5
-	return adc_values[idx];
+uint16_t ADC_GetSteer() {
+	return steer;
+}
+
+uint16_t ADC_GetMotor() {
+	return motor;
+}
+
+uint16_t ADC_GetBreak() {
+	return brek;
+}
+
+uint16_t ADC_GetLight() {
+	return light;
 }
 
 ISR(ADC_vect) {
 	// 현재 채널 값 저장
-	if(current_channel == 3) adc_values[0] = ADC;
-	else if(current_channel == 4) adc_values[1] = ADC;
-	else if(current_channel == 5) adc_values[2] = ADC;
+	if(current_channel == STEER) stear = ADC;
+	else if(current_channel == MOTOR) motor = ADC;
+	else if(current_channel == BREAK) brek = ADC;
+	else if(current_channel == LIGHT) light = ADC;
 
 	// 다음 채널로 이동
-	if(current_channel == 3) current_channel = 4;
-	else if(current_channel == 4) current_channel = 5;
-	else current_channel = 3;
+	if(current_channel == STEER) current_channel = MOTOR;
+	else if(current_channel == MOTOR) current_channel = BREAK;
+	else if(current_channel == BREAK) current_channel = LIGHT;
+	else current_channel = STEER;
 
 	// ADMUX 업데이트
 	ADMUX = (1 << REFS0) | (current_channel & 0x07);
